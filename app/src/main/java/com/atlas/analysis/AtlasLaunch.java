5f7a8d14e44127f917a08be00fe9fccbd395cac3
Anymarvel <314599558@qq.com>
2017-12-19 10:07:54 +0800

    增加AtlasLaunch 使用intent跳转

---
+++ app/src/main/java/com/atlas/analysis/AtlasLaunch.java
@@ -0,0 +1,13 @@
+package com.atlas.analysis;
+
+import android.content.Context;
+import android.taobao.atlas.runtime.AtlasPreLauncher;
+
+
+
+public class AtlasLaunch implements AtlasPreLauncher {
+    @Override
+    public void initBeforeAtlas(Context context) {
+
+    }
+}
--- app/src/main/java/com/atlas/analysis/MainActivity.java
+++ app/src/main/java/com/atlas/analysis/MainActivity.java
@@ -1,5 +1,6 @@
 package com.atlas.analysis;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,18 +11,22 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     private ActivityGroupDelegate mActivityDelegate;
     private ViewGroup mActivityGroupContainer;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = MainActivity.this;
         TextView textView = (TextView) findViewById(R.id.textView);
         mActivityDelegate = new ActivityGroupDelegate(this, savedInstanceState);
         mActivityGroupContainer = (ViewGroup) findViewById(R.id.frame);
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                switchToActivity("home", "com.atlas.firstbundle.FirstBundleActivity");
+                Intent intent = new Intent();
+                intent.setClassName(v.getContext(), "com.atlas.firstbundle.FirstBundle");
+                startActivity(intent);//                switchToActivity("home", "com.atlas.firstbundle.FirstBundleActivity");
             }
         });
     }
